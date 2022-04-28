0db673473ac630245108d1f68e4574dbc7889ee9
Alexander Schmid <Alexander.Schmid@anecon.com>
2022-04-28 17:27:24 +0200

    Add unittest with combinations; add first approved file

--- csharp/Overtime/OvertimeTest/OvertimeApprovalTest.cs
+++ csharp/Overtime/OvertimeTest/OvertimeApprovalTest.cs
@@ -3,6 +3,7 @@ using ApprovalTests.Combinations;
 using ApprovalTests.Reporters;
 using Overtime;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace OvertimeTest
@@ -19,6 +20,22 @@ namespace OvertimeTest
             var overtime = CompensationCalculator.calculateOvertime(1.0m, assignment, briefing);
 
             Approvals.Verify(overtime);
+        }
+
+        [Fact]
+        public void TestOvertimeCalculationCombinations()
+        {
+            var hoursOvertimeTotals = new List<decimal>() { 1.0m, 3.5m, 6.8m };
+            var briefings = new List<Briefing>() { 
+                new Briefing(true, true, true, true), 
+                new Briefing(false, true, true, true), 
+                new Briefing(false, false, true, true) };
+            var assignments = new List<Assignment>() {
+                new Assignment(true, TimeSpan.FromMinutes(1)),
+                new Assignment(true, TimeSpan.FromMinutes(2)),
+                new Assignment(true, TimeSpan.FromMinutes(5)) };
+
+            CombinationApprovals.VerifyAllCombinations(CompensationCalculator.calculateOvertime, hoursOvertimeTotals, assignments, briefings);
 
             //TODO Implement Test
             //Single Verification           -> Approvals.Verify(...);
