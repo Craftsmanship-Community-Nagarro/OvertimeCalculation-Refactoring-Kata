3434f6c16e32cefcbaf1806daac00e13344de894
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2022-08-25 17:13:36 +0200

    refactor: send two conditions to one method

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -5,47 +5,49 @@ import java.time.Duration;
 
 public class CompensationCalculator {
 
-    public final static BigDecimal MAX_OVERTIME_HOURS_RATE_1 = BigDecimal.TEN;
-    public static final int THRESHOLD_OVERTIME_HOURS_RATE_2 = 6;
+  public final static BigDecimal MAX_OVERTIME_HOURS_RATE_1 = BigDecimal.TEN;
+  public static final int THRESHOLD_OVERTIME_HOURS_RATE_2 = 6;
 
-    public static Overtime calculateOvertime(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
-        if (isOvertime(assignment, briefing)) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
-        }
+  public static Overtime calculateOvertime(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
 
-        if (hoursOvertimeTotal.compareTo(BigDecimal.ZERO) < 1) {
-            return new Overtime(BigDecimal.ZERO, BigDecimal.ZERO);
-        }
-
-        if (hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
-        }
-
-        BigDecimal hoursOvertimeRate1 = MAX_OVERTIME_HOURS_RATE_1;
-        BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-        if (assignment.isUnionized()) {
-            BigDecimal threshold = calculateThreshold(assignment);
-            hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-        }
-        return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
+    if (isOverTimeWithTotal(hoursOvertimeTotal, assignment, briefing)) {
+      return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
     }
 
-    private static boolean isOvertime(Assignment assignment, Briefing briefing) {
-        boolean isWatcodeUnion = briefing.watcode() && assignment.isUnionized();
-        boolean isWatcodeNonUnionForeign = briefing.watcode() && !assignment.isUnionized() && briefing.foreign();
-        return (!briefing.watcode() && !briefing.z3() && !assignment.isUnionized())
-                || (briefing.hbmo() && assignment.isUnionized())
-                || isWatcodeNonUnionForeign
-                || isWatcodeUnion
-                || (briefing.foreign() && !assignment.isUnionized());
+    if (hoursOvertimeTotal.compareTo(BigDecimal.ZERO) < 1) {
+      return new Overtime(BigDecimal.ZERO, BigDecimal.ZERO);
     }
 
-    private static BigDecimal calculateThreshold(Assignment assignment) {
-        Duration remainder = assignment.duration().minusHours(CompensationCalculator.THRESHOLD_OVERTIME_HOURS_RATE_2);
-        if (remainder.isNegative()) {
-            return BigDecimal.valueOf(assignment.duration().toSeconds() / 3600);
-        }
-        return BigDecimal.valueOf(CompensationCalculator.THRESHOLD_OVERTIME_HOURS_RATE_2);
+    final BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
+
+    if (assignment.isUnionized()) {
+      BigDecimal threshold = calculateThreshold(assignment);
+      return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2.min(threshold));
     }
+
+    return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
+  }
+
+  private static boolean isOverTimeWithTotal(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
+    return isOvertime(assignment, briefing) || hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1;
+  }
+
+  private static boolean isOvertime(Assignment assignment, Briefing briefing) {
+    boolean isWatcodeUnion = briefing.watcode() && assignment.isUnionized();
+    boolean isWatcodeNonUnionForeign = briefing.watcode() && !assignment.isUnionized() && briefing.foreign();
+    return (!briefing.watcode() && !briefing.z3() && !assignment.isUnionized())
+            || (briefing.hbmo() && assignment.isUnionized())
+            || isWatcodeNonUnionForeign
+            || isWatcodeUnion
+            || (briefing.foreign() && !assignment.isUnionized());
+  }
+
+  private static BigDecimal calculateThreshold(Assignment assignment) {
+    Duration remainder = assignment.duration().minusHours(CompensationCalculator.THRESHOLD_OVERTIME_HOURS_RATE_2);
+    if (remainder.isNegative()) {
+      return BigDecimal.valueOf(assignment.duration().toSeconds() / 3600);
+    }
+    return BigDecimal.valueOf(CompensationCalculator.THRESHOLD_OVERTIME_HOURS_RATE_2);
+  }
 
 }
